0bae8b94cf261b6429b76517590df4d908d15c31
Northarea <adsl41c@yandex.ru>
2020-07-14 15:47:52 +0300

    update

--- tests/DollarTest.java
+++ tests/DollarTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class DollarTest {
     @Test
-    void Multiplication() {
+    void name() {
         Dollar five = new Dollar(5);
         five.times(2);
         assertEquals(10, five.amount);
